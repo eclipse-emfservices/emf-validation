041f74712cbea7ffa652ef247e8ccfe7b77869a1
cmcgee <cmcgee>
2006-04-27 01:23:06 +0000

    [132018] emft_head cmcgee 060425 Create a example zipper plugin to be included with the SDK for EMFT components

--- org.eclipse.emf.validation.examples/src/org/eclipse/emf/validation/examples/internal/wizard/AdapterExampleWizard.java
+++ org.eclipse.emf.validation.examples/src/org/eclipse/emf/validation/examples/internal/wizard/AdapterExampleWizard.java
@@ -12,10 +12,10 @@ public class AdapterExampleWizard
 		// We need the adapter example to be unzipped along with the
 		// EMF library example model, edit and editor examples
 		List projects = new ArrayList(4);
-		projects.add(new ProjectDescriptor("org.eclipse.emf.validation.examples", "zips/adapter.zip", "org.eclipse.emf.validation.examples.adapter")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 		projects.add(new ProjectDescriptor("org.eclipse.emf.validation.examples", "zips/library.zip", "org.eclipse.emf.examples.library"));  //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$
 		projects.add(new ProjectDescriptor("org.eclipse.emf.validation.examples", "zips/libraryEdit.zip", "org.eclipse.emf.examples.library.edit")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 		projects.add(new ProjectDescriptor("org.eclipse.emf.validation.examples", "zips/libraryEditor.zip", "org.eclipse.emf.examples.library.editor"));  //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$
+		projects.add(new ProjectDescriptor("org.eclipse.emf.validation.examples", "zips/adapter.zip", "org.eclipse.emf.validation.examples.adapter")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 		
 		return projects;
 	}
