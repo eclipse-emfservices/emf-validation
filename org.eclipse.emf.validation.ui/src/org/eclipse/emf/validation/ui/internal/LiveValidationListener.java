52c11f473712a0ee9c0bbd50fb0e9dbfba687603
cdamus <cdamus>
2007-03-30 19:23:32 +0000

    Fix trivial warnings

--- org.eclipse.emf.validation.ui/src/org/eclipse/emf/validation/ui/internal/LiveValidationListener.java
+++ org.eclipse.emf.validation.ui/src/org/eclipse/emf/validation/ui/internal/LiveValidationListener.java
@@ -43,7 +43,6 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
