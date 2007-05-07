fcfb91fe3bde92afcea7fcb7b1d777dfa90bd76d
cdamus <cdamus>
2007-05-07 16:12:12 +0000

    [165019] Complete Validation Programmer's Guide Update OCL example to demonstrate custom constraint provider

--- org.eclipse.emf.validation.examples.ocl/src/org/eclipse/emf/validation/examples/ocl/OCLValidationExamplePlugin.java
+++ org.eclipse.emf.validation.examples.ocl/src/org/eclipse/emf/validation/examples/ocl/OCLValidationExamplePlugin.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2005 IBM Corporation and others.
+ * Copyright (c) 2005, 2007 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,12 +12,14 @@
  *
  * </copyright>
  *
- * $Id: OCLValidationExamplePlugin.java,v 1.1 2006/02/13 16:12:15 cdamus Exp $
+ * $Id: OCLValidationExamplePlugin.java,v 1.2 2007/05/07 16:12:12 cdamus Exp $
  */
 
 package org.eclipse.emf.validation.examples.ocl;
 
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Plugin;
+import org.eclipse.core.runtime.Status;
 import org.osgi.framework.BundleContext;
 
 /**
@@ -36,6 +38,15 @@ public class OCLValidationExamplePlugin extends Plugin {
 		plugin = this;
 	}
 
+	public static String getID() {
+		return getDefault().getBundle().getSymbolicName();
+	}
+	
+	public static void log(String message, Throwable t) {
+		getDefault().getLog().log(new Status(IStatus.ERROR, getID(),
+				1, message, t));
+	}
+	
 	/**
 	 * This method is called upon plug-in activation
 	 */
