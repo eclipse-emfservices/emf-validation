acbe4e9fbfeb0d8da19898bc71b37fb6c24503e5
Anthony Hunter <anthonyh@ca.ibm.com>
2016-06-07 11:44:48 -0400

    Fix publish script bug with BUILD_TYPE=R parameter from Hudson

--- org.eclipse.emf.validation.repository/emf-validation-hipp-publish.sh
+++ org.eclipse.emf.validation.repository/emf-validation-hipp-publish.sh
@@ -66,8 +66,8 @@ remoteUpdateSiteBase="modeling/emf/validation/updates/$remoteSite"
 remoteUpdateSite="/home/data/httpd/download.eclipse.org/$remoteUpdateSiteBase"
 echo "`date +%Y-%m-%d-%H:%M:%S` Publishing to remote update-site: $remoteUpdateSite"
 
-if [ -z "$dropFilesLabel" -a "$buildType" != i  -a "$buildType" != I ]; then
-	echo "Please provide a drop files label to append to the version (e.g. M5, RC1) if this is not an I build."
+if [ -z "$dropFilesLabel" -a "$buildType" != i  -a "$buildType" != I -a "$buildType" != r  -a "$buildType" != R]; then
+	echo "Please provide a drop files label to append to the version (e.g. M5, RC1) if this is not an I or R build."
 	exit 0
 fi
 
