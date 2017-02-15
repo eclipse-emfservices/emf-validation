31b800d7bb66b767bcdf38d262c21cc65f2d2679
Pierre-Charles David <pierre-charles.david@obeo.fr>
2017-02-15 19:42:44 +0100

    [releng] Updated eclipse-SDK to 4.6.2
    Change-Id: I2f148e80b743bfcbc77fb84f5671ffc2a46d1658
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- org.eclipse.emf.validation.repository/emf-validation-hipp-publish.sh
+++ org.eclipse.emf.validation.repository/emf-validation-hipp-publish.sh
@@ -80,8 +80,8 @@ echo "`date +%Y-%m-%d-%H:%M:%S` Working in `pwd`"
 
 # Download and prepare Eclipse SDK, which is needed to process the update site
 echo "`date +%Y-%m-%d-%H:%M:%S` Downloading eclipse to $PWD"
-cp /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4/R-4.5.2-201602121500/eclipse-SDK-4.5.2-linux-gtk-x86_64.tar.gz .
-tar -xzf eclipse-SDK-4.5.2-linux-gtk-x86_64.tar.gz
+cp /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4/R-4.6.2-201611241400/eclipse-SDK-4.6.2-linux-gtk-x86_64.tar.gz .
+tar -xzf eclipse-SDK-4.6.2-linux-gtk-x86_64.tar.gz
 cd eclipse
 chmod 700 eclipse
 cd ..
@@ -93,7 +93,7 @@ fi
 echo "`date +%Y-%m-%d-%H:%M:%S` Installing WTP Releng tools"
 ./eclipse/eclipse -nosplash --launcher.suppressErrors -clean -debug -application org.eclipse.equinox.p2.director -repository http://download.eclipse.org/webtools/releng/repository/ -installIUs org.eclipse.wtp.releng.tools.feature.feature.group
 # Clean up
-rm eclipse-SDK-4.5.2-linux-gtk-x86_64.tar.gz
+rm eclipse-SDK-4.6.2-linux-gtk-x86_64.tar.gz
 
 # Generate drop files
 echo "`date +%Y-%m-%d-%H:%M:%S` Converting update site to runnable form"
